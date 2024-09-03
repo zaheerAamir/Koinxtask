2a4c3546001931371aecafaca1f35a63d8329adc
zaheerAamir <aamirzaheer95@gmail.com>
2024-09-03 21:23:38 +0530

    desc: Resolving cron deployment error1

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -109,7 +109,7 @@ export async function updateEthPriceController() {
     };
 
     const res = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=inr", options);
-    console.log(res)
+    console.log(res.status)
     const data = await res.json();
 
     updateEthPriceService(data, address);
